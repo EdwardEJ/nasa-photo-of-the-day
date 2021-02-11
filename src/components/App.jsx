621948c3f417c6f35aa983111abd0c1af76e1e3f
A <jesus.e.estrada@hotmail.com>
2021-02-10 19:30:56 -0600

    testing change

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,28 @@
 import React, { useState } from 'react';
 import '../styles/App.css';
-import DisplayInfo from './DisplayInfo'
-import Searchbar from './Searbar'
+import DisplayInfo from './DisplayInfo';
+import Searchbar from './Searbar';
 
-import { Grid } from '@material-ui/core'
+import { Grid } from '@material-ui/core';
 
-import { useSetInfo } from '../hooks/useSetInfo'
+import { useSetInfo } from '../hooks/useSetInfo';
 
 function App() {
-  const [currentDate, setCurrentDate] = useState('')
-  const [info] = useSetInfo(currentDate)
+	const [currentDate, setCurrentDate] = useState('');
+	const [info] = useSetInfo(currentDate);
 
-  return (
-    <Grid container direction='column'>
-      <Grid item xs={12}>
-        <Searchbar onDate={(newDate) => setCurrentDate(newDate)} />
-      </Grid>
-      <Grid item>
-        <DisplayInfo info={info} />
-      </Grid>
-    </Grid>
-  );
+	console.log('testing change');
+
+	return (
+		<Grid container direction='column'>
+			<Grid item xs={12}>
+				<Searchbar onDate={(newDate) => setCurrentDate(newDate)} />
+			</Grid>
+			<Grid item>
+				<DisplayInfo info={info} />
+			</Grid>
+		</Grid>
+	);
 }
 
 export default App;
